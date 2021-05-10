3c22e03953af8fbb6dea7f4e7722578e75c36c47
flavioespinoza <flavio.espinoza@gmail.com>
2021-05-10 03:18:41 -0600

    Allowed characters

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-# myni.me
-
-Minify your URL's
-
-```js
 const allowed = {
   characters:
     '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-áéíóúüñАаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯяІіѲѳѢѣѴѵœ∑´®†¥¨ˆøπ“‘«`åß∂ƒ©˙∆˚¬…æΩ≈ç√∫˜µ≤≥÷¡™£¢∞§¶•ªº–≠',
 }
-```
+
